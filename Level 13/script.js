39f24b19674d9bc3a150bbf2c82fc5d73dfa968b
jilljoyce <46486508+jilljoyce@users.noreply.github.com>
2021-04-11 16:06:57 +0200

    Delete Level 13 directory

--- Level 13/script.js
+++
@@ -1,6 +0,0 @@
-console.log("Hallo Winc Academy Studenten");
-/*let bigSum = 1230941 * 1823794;
-console.log("Antwoord berekening: ", bigSum);
-let bigDivide = 637263 / 54;
-console.log("Antwoord delen door: ", bigDivide);*/
-// dit is een grote som
